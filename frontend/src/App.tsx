8050cefbd1c5dfa0fa47db268a63143fe5c3f1f8
Martin <okapiposter@gmail.com>
2024-04-08 20:00:02 +0200

    modify stylings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import {Route, Routes} from "react-router-dom";
-import PlantGallery from "./components/PlantGallery.tsx";
-import usePlants from "./hooks/usePlants.ts";
-import NewPlantForm from "./components/NewPlantForm.tsx";
-import {PlantDto} from "./types/PlantDto.ts";
-import React from "react";
-import PlantDetails from "./components/PlantDetails.tsx";
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import PlantGallery from './components/PlantGallery'; // Stelle sicher, dass die Pfade zu deinen Komponenten korrekt sind
+import NewPlantForm from './components/NewPlantForm';
+import PlantDetails from './components/PlantDetails';
+import NavBar from './components/NavBar';
+import usePlants from './hooks/usePlants';
+import { PlantDto } from './types/PlantDto'; // Pfad zu PlantDto anpassen
 
 export default function App() {
-    const {plants, savePlant, updatePlant, deletePlant} = usePlants();
+    const { plants, savePlant, updatePlant, deletePlant } = usePlants();
 
     const handleSavePlant = (e: React.FormEvent, formData: PlantDto) => {
         e.preventDefault();
@@ -16,14 +16,13 @@ export default function App() {
     };
 
     return (
-
-        <Routes>
-            <Route path={"/"} element={<PlantGallery plants={plants}/>}/>
-            <Route path={"/new"} element={<NewPlantForm savePlant={handleSavePlant}/>}/>
-            <Route path={"/plants/:id"} element={<PlantDetails updatePlant={updatePlant} deletePlant={deletePlant}/>}/>
-        </Routes>
-
-    )
+        <>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<PlantGallery plants={plants} />} />
+                <Route path="/new" element={<NewPlantForm savePlant={handleSavePlant} />} />
+                <Route path="/plants/:id" element={<PlantDetails updatePlant={updatePlant} deletePlant={deletePlant} />} />
+            </Routes>
+        </>
+    );
 }
-
-
