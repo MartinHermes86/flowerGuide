6adcc2cf40e1fc35e10701e4675a6f58eb07c76c
Martin <okapiposter@gmail.com>
2024-04-16 15:07:06 +0200

    edit font, fix navbar, remove app.css

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 export default function NavBar(){
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">FlowerGuide</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
